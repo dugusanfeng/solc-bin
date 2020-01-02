4e8a8e671ad65ee8625c409afe38e3e47fb2d0c6
steven <18640802428@163.com>
2020-01-02 14:57:47 +0800

    Added compiler version v0.5.12+commit.6684895d

---
+++ bin/list.js
@@ -0,0 +1,13 @@
+
+var soljsonSources = [
+  "soljson-v0.5.12+commit.6684895d.js"
+];
+var soljsonReleases = {
+  "0.5.12": "soljson-v0.5.12+commit.6684895d.js"
+};
+
+if (typeof(module) !== 'undefined')
+  module.exports = {
+    'allVersions': soljsonSources,
+    'releases': soljsonReleases
+  };
