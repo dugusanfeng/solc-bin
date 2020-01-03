bf8bf8753cd3fd630bbf6072a4495ba7e2654024
steven <18640802428@163.com>
2020-01-03 15:53:11 +0800

    Added compiler version v0.4.14+commit.c2215d46

--- bin/list.js
+++ bin/list.js
@@ -24,7 +24,8 @@ var soljsonSources = [
   "soljson-v0.4.18+commit.9cf6e910.js",
   "soljson-v0.4.17+commit.bdeb9e52.js",
   "soljson-v0.4.16+commit.d7661dd9.js",
-  "soljson-v0.4.15+commit.8b45bddb.js"
+  "soljson-v0.4.15+commit.8b45bddb.js",
+  "soljson-v0.4.14+commit.c2215d46.js"
 ];
 var soljsonReleases = {
   "0.5.13": "soljson-v0.5.13+commit.9ff23752.js",
@@ -51,7 +52,8 @@ var soljsonReleases = {
   "0.4.18": "soljson-v0.4.18+commit.9cf6e910.js",
   "0.4.17": "soljson-v0.4.17+commit.bdeb9e52.js",
   "0.4.16": "soljson-v0.4.16+commit.d7661dd9.js",
-  "0.4.15": "soljson-v0.4.15+commit.8b45bddb.js"
+  "0.4.15": "soljson-v0.4.15+commit.8b45bddb.js",
+  "0.4.14": "soljson-v0.4.14+commit.c2215d46.js"
 };
 
 if (typeof(module) !== 'undefined')
