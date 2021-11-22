4b8d943abe3ef195035a45dc414040a57cfcc654
xuwen <xuwen@juzix.net>
2021-11-22 15:35:01 +0800

    Update soljson binary (adapt to 1.1.1 version platon network)

--- bin/list.js
+++ bin/list.js
@@ -1,17 +1,17 @@
 
 var soljsonSources = [
-  "soljson-v0.8.2+commit.b770a684.js",
-  "soljson-v0.7.6+commit.c217223d.js",
-  "soljson-v0.6.12+commit.4cffc863.js",
-  "soljson-v0.5.17+commit.b6e443d8.js",
-  "soljson-v0.4.26+commit.60076b06.js"
+  "soljson-v0.8.6+commit.915f2fcc.js",
+  "soljson-v0.7.6+commit.6cb3c108.js",
+  "soljson-v0.6.12+commit.8946da9f.js",
+  "soljson-v0.5.17+commit.40d7687e.js",
+  "soljson-v0.4.26+commit.ecbb1517.js"
 ];
 var soljsonReleases = {
-  "0.8.2": "soljson-v0.8.2+commit.b770a684.js",
-  "0.7.6": "soljson-v0.7.6+commit.c217223d.js",
-  "0.6.12": "soljson-v0.6.12+commit.4cffc863.js",
-  "0.5.17": "soljson-v0.5.17+commit.b6e443d8.js",
-  "0.4.26": "soljson-v0.4.26+commit.60076b06.js"
+  "0.8.6": "soljson-v0.8.6+commit.915f2fcc.js",
+  "0.7.6": "soljson-v0.7.6+commit.6cb3c108.js",
+  "0.6.12": "soljson-v0.6.12+commit.8946da9f.js",
+  "0.5.17": "soljson-v0.5.17+commit.40d7687e.js",
+  "0.4.26": "soljson-v0.4.26+commit.ecbb1517.js"
 };
 
 if (typeof(module) !== 'undefined')
